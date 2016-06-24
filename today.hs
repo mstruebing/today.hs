e4ec3da0ac72bebe03729330d058d3c2ad8576db
Max Strübing <max_struebing@gmx.de>
2016-06-24 10:37:08 +0200

    Add comments and remove unnecessary type classes

--- today.hs
+++ today.hs
@@ -1,18 +1,11 @@
 import Data.Time.Clock.POSIX(getPOSIXTime)
 
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
-    deriving (Eq, Ord, Show, Read, Bounded, Enum)
+    deriving (Show, Bounded, Enum)
 
 main :: IO ()
 main = do
     timestamp <- getCurrentTimestamp
---    putStrLn 
---        (show 
---            (getDayList !! 
---            (fromIntegral 
---            (calculateCurrentDayIndex
---            (calculateElapsedDays timestamp
---            )))))
     putStrLn 
         (show 
             (getDayList !! (fromIntegral $ 
@@ -20,17 +13,20 @@ main = do
                             calculateElapsedDays $ 
                             timestamp)))
 
+-- create a list with all days in order
 getDayList :: [Day]
 getDayList = [minBound .. maxBound] :: [Day]
 
+-- returns the current timestamp
 getCurrentTimestamp :: IO Integer
 getCurrentTimestamp = (round `fmap` getPOSIXTime)
 
+-- calculate the elapsed days since 01.01.1970
 calculateElapsedDays:: Integer-> Integer
 calculateElapsedDays ts = ts `div` 86400
 
 
--- add 3 beacuse the 1.1.1970 was a Thursday
+-- add 3 beacuse the 01.01.1970 was a Thursday
 calculateCurrentDayIndex :: Integer-> Integer
 calculateCurrentDayIndex x = (x + 3) `mod` 7
 
