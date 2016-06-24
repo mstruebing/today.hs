434c5144c6d93c949edc45e0e6b2759deafd0282
Max Strübing <max_struebing@gmx.de>
2016-06-24 13:16:08 +0200

    Add basic test and let make test before build

--- Makefile
+++ Makefile
@@ -2,9 +2,13 @@ COMPILER = ghc -Wall
 
 PROGNAME = Today
 LOWER_PROGNAME = $(shell echo $(PROGNAME) | tr A-Z a-z)
+TEST = Test.hs
 MAIN = Main
 
-all: target clean
+all: test target clean
+
+test: $(PROGNAME).hs $(MAIN).hs $(Test)
+	runhaskell $(TEST)
 
 target: $(PROGNAME).hs $(MAIN).hs  
 	$(COMPILER) -o $(LOWER_PROGNAME) $(MAIN).hs 
---
+++ Test.hs
@@ -0,0 +1,12 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+import Today
+
+main :: IO ()
+main = hspec $ do
+    describe "Today" $ do
+        it "get the right weekday from a timestamp" $ do
+            getDayFromTimestamp 0 `shouldBe` Thursday
+            getDayFromTimestamp 1466766758 `shouldBe` Friday
